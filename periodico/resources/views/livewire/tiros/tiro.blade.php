31e3b4ada47731feedcd28d4a0d4d700999636d8
Kevin López González <86860306+Nightmare289@users.noreply.github.com>
2022-07-01 16:57:56 -0500

    update

--- periodico/resources/views/livewire/tiros/tiro.blade.php
+++ periodico/resources/views/livewire/tiros/tiro.blade.php
@@ -308,7 +308,34 @@
 
         </x-jet-dialog-modal>
 
+        <x-jet-dialog-modal>
 
+            <x-slot name="title">
+                <div class="flex sm:px-6">
+                    <h1 class="mb-3 text-2xl text-black font-bold ml-3">Historial de Remisión</h1>
+                    <button type="button" wire:click="hideModalRemision" wire:loading.attr="disabled"
+                        class="mb-3 text-gray-400 bg-transparent hover:bg-red-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-red-600 dark:hover:text-white"
+                        data-modal-toggle="defaultModal">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                <hr>
+            </x-slot>
+
+            <x-slot name="content">
+
+            </x-slot>
+
+            <x-slot name="footer">
+
+            </x-slot>
+
+        </x-jet-dialog-modal>
 
     </div>
 </div>
