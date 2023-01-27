0c0171b815789bff058e96d97b86d29fd3b1fa2d
nightmare28899 <ninja_gaiden28899@hotmail.com>
2023-01-27 01:27:46 -0600

    fix

--- periodico/app/Http/Livewire/Factura.php
+++ periodico/app/Http/Livewire/Factura.php
@@ -47,7 +47,7 @@ class Factura extends Component
         if (substr($idTipo, 0, 6) == 'suscri') {
 
             $this->cliente = Cliente::find($cliente_id);
-            $this->suscripcion = Suscripcion::where('idSuscripcion', $idTipo)->first();
+            $this->suscripcion = Suscripcion::where('idSuscripcion', $idTipo)->where('contrato', '!=', 'Cortesía')->first();
             $this->domicilio = domicilioSubs::where('id', $this->suscripcion['domicilio_id'])->first();
             $this->tipoFactura = 'PUE';
         } else if (substr($idTipo, 0, 5) == 'venta') {
